f6a7fd5b96b625199816a145cacf2a31559550e0
51214-Tasawar <tasawarabbasbuck9@gmail.com>
2025-11-29 23:56:13 +0500

    logic implemention

--- Js Files/Index.js
+++ Js Files/Index.js
@@ -1,3 +1,38 @@
-fetch(Cousre.json)
-.then((data)=>data.json())
-.then()
+fetch("Cousre.json")
+.then((Course)=>Course.json())
+.then(data=>{
+    data.map((course)=>(
+    document.getElementById("Courses").innerHTML = `
+ <div  style=" width: 330px; height: 390px; background-color: antiquewhite ; border-radius: 10px;">
+        <div style="display: flex; width: 100%; height: 45%">
+          <img src=${course.image}/>
+        </div>
+        <div style="display: flex;flex-direction: column;width: 100%;justify-content: flex-start;
+            padding: 10px; gap: 20px;">
+          <p style=" width: 40%;height: max-content;border: 2px solid; border-radius: 5px;
+              text-align: center;">${course.Course}</p>
+          <p style="font-size: 20px; font-weight: 500">by ${course.Tutorname}</p>
+          <div style="display: flex; justify-content: space-evenly">
+            <div style="display: flex; display: inline; gap: 10px">
+              <i class="text-yellow-400 fa fa-star"></i>
+              <p>${course.rating}</p>
+            </div>
+            <div style="display: flex; gap: 10px">
+              <i class="fa fa-users"></i>
+              <p>${course.member}</p>
+            </div>
+            <div style="display: flex; display: inline; gap: 10px">
+              <i class="fa-regular fa-clock"></i>
+              <p>${course.CourseDuration}</p>
+            </div>
+          </div>
+          <div style="display: flex; justify-content: center">
+            <button style="width: 80%;height: 30px;background-color: blue;
+                border-radius: 10px;color: white;cursor: pointer;">
+              Start Course
+            </button>
+          </div>
+        </div>
+      </div> `
+    ))
+})
